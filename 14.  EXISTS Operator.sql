4aa80acd5ba191302edbf42e9fb134bfc66475f2
cmcataldo <cmcataldo6@gmail.com>
2025-04-21 19:02:37 +0800

    Delete 14.  EXISTS Operator.sql

--- 14.  EXISTS Operator.sql
+++
@@ -1,7 +0,0 @@
-------------------------------------------------------------------------------------------------------------------------------------
--- 14.  EXISTS Operator
-------------------------------------------------------------------------------------------------------------------------------------
--- Uses EXISTS to find products that have been sold at least once.
-SELECT ProductID, Name
-FROM Production.Product AS p
-WHERE EXISTS (SELECT 1 FROM Sales.SalesOrderDetail AS sod WHERE p.ProductID = sod.ProductID);
