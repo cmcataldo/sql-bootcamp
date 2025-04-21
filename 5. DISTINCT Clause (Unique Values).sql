6a6c2165c87ce5dfce485e4261ec49cbe6013b84
cmcataldo <cmcataldo6@gmail.com>
2025-04-21 19:04:15 +0800

    Delete 5. DISTINCT Clause (Unique Values).sql

--- 5. DISTINCT Clause (Unique Values).sql
+++
@@ -1,6 +0,0 @@
-------------------------------------------------------------------------------------------------------------------------------------
--- 5. DISTINCT Clause (Unique Values)
-------------------------------------------------------------------------------------------------------------------------------------
--- Retrieves all distinct (unique) values from the Title column in the Person.Person table.
-SELECT DISTINCT Title
-FROM Person.Person;
