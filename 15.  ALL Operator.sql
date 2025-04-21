ab3119db344a0d22f7e7b62606d5ec6e4974248e
cmcataldo <cmcataldo6@gmail.com>
2025-04-21 19:02:47 +0800

    Delete 15.  ALL Operator.sql

--- 15.  ALL Operator.sql
+++
@@ -1,7 +0,0 @@
-------------------------------------------------------------------------------------------------------------------------------------
--- 15.  ALL Operator
-------------------------------------------------------------------------------------------------------------------------------------
--- ALL: Find products with a ListPrice greater than ALL of the ListPrices of product category 14.
-SELECT ProductID, Name, ListPrice
-FROM Production.Product
-WHERE ListPrice > ALL (SELECT ListPrice FROM Production.Product WHERE ProductID = 14);
