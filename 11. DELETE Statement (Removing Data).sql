1b4514ef6f51f2e1ed06cd816028ec18dfbeb6fa
cmcataldo <cmcataldo6@gmail.com>
2025-04-21 19:01:42 +0800

    Delete 11. DELETE Statement (Removing Data).sql

--- 11. DELETE Statement (Removing Data).sql
+++
@@ -1,9 +0,0 @@
-------------------------------------------------------------------------------------------------------------------------------------
--- 11. DELETE Statement (Removing Data)
-------------------------------------------------------------------------------------------------------------------------------------
--- Deletes the row we inserted.  Use WHERE clause to specify which rows to delete!
-DELETE FROM Person.Person
-WHERE LastName = 'Doe';
-
--- Verify the delete
-SELECT * FROM Person.Person WHERE LastName = 'Doe';
